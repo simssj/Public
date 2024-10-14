898f7b11975b1ac1ea9a8762d505d98d2a57930a
Steve Sims <simssj@gmail.com>
2024-10-14 12:20:39 -0400

    Update fn_Messages.sh

--- Function Templates/fn_Messages.sh
+++ Function Templates/fn_Messages.sh
@@ -1,24 +1,26 @@
 ############################# fn_msg_ functions ##########################################
-function fn_msg_Info() { # Prints a generic failure message. Try to keep it under ~70 characters
-  printf "\r\e[2K   [\e[93mi\e[0m] %s\n" "$@"
+function fn_msg_Debug() { # Prints a provided 'debug' message. Try to keep it under ~70 characters
+	[[ ${_INTERACTIVE} == True && -n "$Debug" ]] && printf "\r\e[2K   [\e[93m+\e[0m] "
+	[[ -n "$Debug" ]] && printf "%s\n" "$@"
 } # fn_msg_Info
-#
 
+function fn_msg_Info() { # Prints a provided 'info' message. Try to keep it under ~70 characters
+	[[ ${_INTERACTIVE} == True ]] && printf "\r\e[2K   [\e[93mi\e[0m] "
+	printf "%s\n" "$@"
+} # fn_msg_Info
 
-function fn_msg_Status() { # Prints a generic status message. Try to keep it under ~70 characters
-  printf "\r\e[2K   [ ] %s\n" "$@"
+function fn_msg_Status() { # Prints a provided 'status' message. Try to keep it under ~70 characters
+  [[ ${_INTERACTIVE} == True ]] && printf "\r\e[2K       "
+	printf "%s\n" "$@"
 } # fn_msg_Status
-#
 
-
-function fn_msg_Success() { # Moves the cursor up one line and then prints a generic success message. Try to keep it under ~70 characters
-  printf "\r$'\e'[A\r\e[2K   [\e[92m\xe2\x9c\x94\e[0m] %s\n" "$@"
+function fn_msg_Success() { # Prints a provided 'success' message. Try to keep it under ~70 characters
+	[[ ${_INTERACTIVE} == True ]] && { sleep 1; printf "\r\e[2K   ✅ "; }
+	printf "%s\n" "$@"
 } # fn_msg_Success
-#
 
-
-function fn_msg_Failure() { # Prints a generic failure message. Try to keep it under ~70 characters
-  printf "\r\e[2K   [\e[91m\xe2\x9d\x8c\e[0m] %s\n" "$@"
+function fn_msg_Failure() { # Prints a provided 'failure' message. Try to keep it under ~70 characters
+  [[ ${_INTERACTIVE} == True ]] && printf "\r\e[2K   [\e[91m\xe2\x9d\x8c\e[0m] "
+	printf "%s\n" "$@"
 } # fn_msg_Failure
-#
 ############################# End of fn_msg_ functions ######################################
