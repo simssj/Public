137c9731659a1f4092c474947a13b870d3c48e38
Steve Sims <simssj@gmail.com>
2025-03-20 06:34:57 -0700

    Update fn_Messages.sh

--- Function Templates/fn_Messages.sh
+++ Function Templates/fn_Messages.sh
@@ -1,26 +1,33 @@
 ############################# fn_msg_ functions ##########################################
 function fn_msg_Debug() { # Prints a provided 'debug' message. Try to keep it under ~70 characters
-	[[ ${_INTERACTIVE} == True && -n "$Debug" ]] && printf "\r\e[2K   [\e[93m+\e[0m] "
-	[[ -n "$Debug" ]] && printf "%s\n" "$@"
+	[[ -n ${_INTERACTIVE} && -n "${_DEBUG}" ]] && printf "\r\e[2K   [\e[93m*\e[0m] "
+	[[ -n "${_DEBUG}" ]] && printf "%s\n" "$@"
 } # fn_msg_Info
 
 function fn_msg_Info() { # Prints a provided 'info' message. Try to keep it under ~70 characters
-	[[ ${_INTERACTIVE} == True ]] && printf "\r\e[2K   [\e[93mi\e[0m] "
+	[[ -n ${_INTERACTIVE} ]] && printf "\r\e[2K   [\e[93mi\e[0m] "
 	printf "%s\n" "$@"
 } # fn_msg_Info
 
 function fn_msg_Status() { # Prints a provided 'status' message. Try to keep it under ~70 characters
-  [[ ${_INTERACTIVE} == True ]] && printf "\r\e[2K       "
+  [[ -n ${_INTERACTIVE} ]] && printf "\r\e[2K       "
 	printf "%s\n" "$@"
 } # fn_msg_Status
 
 function fn_msg_Success() { # Prints a provided 'success' message. Try to keep it under ~70 characters
-	[[ ${_INTERACTIVE} == True ]] && { sleep 1; printf "\r\e[2K   ✅ "; }
+	[[ -n ${_INTERACTIVE} ]] && { sleep 1; printf "\r\e[2K   [✅] "; }
 	printf "%s\n" "$@"
 } # fn_msg_Success
 
 function fn_msg_Failure() { # Prints a provided 'failure' message. Try to keep it under ~70 characters
-  [[ ${_INTERACTIVE} == True ]] && printf "\r\e[2K   [\e[91m\xe2\x9d\x8c\e[0m] "
+  printf "\r\e[2K   [\e[91m\xe2\x9d\x8c\e[0m] "
 	printf "%s\n" "$@"
 } # fn_msg_Failure
+
+function fn_msg_Multiline() { # Prints a provided 'multi-line string' with correct left (tab) indent(s)
+	echo "$@" | while IFS= read -r line; do printf "\t%s\n " "$line" ; done
+# The `sed` way doesn't deal well with the first line indentation
+#  echo "     $@" | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\n        /g'
+} # fn_msg_Multiline
+
 ############################# End of fn_msg_ functions ######################################
